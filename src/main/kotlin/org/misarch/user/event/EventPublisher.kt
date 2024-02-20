85ca982c25a4179fc83e35751c5ae938f888b005
nk-coding <niklask.coding@gmail.com>
2024-02-20 04:04:19 +0100

    remove automatic prefixing

--- src/main/kotlin/org/misarch/user/event/EventPublisher.kt
+++ src/main/kotlin/org/misarch/user/event/EventPublisher.kt
@@ -14,11 +14,11 @@ class EventPublisher(private val client: DaprClient) {
      * Publishes an event via the Dapr client.
      * Uses the pubsub component.
      *
-     * @param topic the topic to publish to, automatically prefixed with "/user"
+     * @param topic the topic to publish to
      * @param message the message to publish
      */
     suspend fun publishEvent(topic: String, message: Any) {
-        client.publishEvent(UserEvents.PUBSUB_NAME, "user/$topic", message).awaitSingleOrNull()
+        client.publishEvent(UserEvents.PUBSUB_NAME, topic, message).awaitSingleOrNull()
     }
 
 }
